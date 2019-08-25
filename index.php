7649d9dcc350e2f93e886a9e1666ff03e61fb407
Ariston Lorenzo <ariston.thomas@gmail.com>
2019-08-24 20:16:58 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!--
+ ________ ___  ________  _______
+|\  _____\\  \|\   __  \|\  ___ \
+\ \  \__/\ \  \ \  \|\  \ \   __/|          index.php
+ \ \   __\\ \  \ \   _  _\ \  \_|/__
+  \ \  \_| \ \  \ \  \\  \\ \  \_|\ \       Updated by Ariston Lorenzo on 6/18/2019.
+   \ \__\   \ \__\ \__\\ _\\ \_______\
+    \|__|    \|__|\|__|\|__|\|_______|      Copyright © 2019 FiRe Tech (on behalf of 'myst development' and 'LScar Studios').
+-->
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="origin" name="referrer">
+        <meta name="keywords" content="FiRe, fire, firesearch, FiReSearch, FireSearch, FiReTech, FireTech, FiReTechnologies, FireTechnologies, FiRe Technologies, Fire Technologies">
+        <meta name="description" content="Search the web with a safe and modern engine!">
+        <meta name="subject" content="A search engine made to give you friendly an safe user experience.">
+        <meta name="copyright" content="<copyright>Copyright © 2019 FiRe Technologies (on behalf of 'myst development' and 'LScar Studios'). All rights reserved.</copyright>">
+        <meta name="language" content="english">
+        <meta name="author" content="Ariston Lorenzo, ariston.thomas@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Ariston Thomas Lorenzo, CEO">
+        <meta name="author" content="Levi Scott Hicks, tbnrscar@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Levi Scott Hicks, CTO">
+        <meta name="designer" content="Ariston Lorenzo, ariston.thomas@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Ariston Thomas Lorenzo, CEO">
+        <meta name="reply-to" content="mail@firesearch.co">
+        <meta name="owner" content="Ariston Lorenzo, ariston.thomas@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Ariston Thomas Lorenzo, CEO">
+        <meta name="owner" content="Levi Scott Hicks, tbnrscar@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Levi Scott Hicks, CTO">
+        <meta name="url" content="http://firesearch.co">
+        <meta name="indentifier-url" content="http://firesearch.co">
+
+        <!-- Apple Metadata -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-touch-fullscreen" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="blue">
+        <meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=320, initial-scale=2.3, user-scalable=no">
+        <meta name="viewport" content="width = device-width">
+        <meta name="viewport" content="initial-scale = 1.0">
+        <meta name="viewport" content="initial-scale = 2.3, user-scalable=no">
+
+        <!-- Internet Explorer Metadata -->
+        <meta http-equiv="Page-Enter" content="RevealTrans(Duration=2.0,Transition=2)">
+        <meta http-equiv="Page-Exit" content="RevealTrans(Duration=3.0,Transition=12)">
+        <meta name="mssmarttagspreventparsing" content="true">
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
+        <meta name="msapplication-starturl" content="http://firesearch.co">
+        <meta name="msapplication-window" content="width=800;height=600">
+        <meta name="msapplication-navbutton-color" content="red">
+        <meta name="application-name" content="FiReSearch">
+        <meta name="msapplication-tooltip" content="Launch FiReSearch">
+
+        <title> FiRe </title>
+
+        <link rel="apple-touch-icon" href="http://firesearch.co/api/imgs/FiRe.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="http://firesearch.co/api/imgs/FiRe.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="http://firesearch.co/api/imgs/FiRe.png">
+        <link rel="apple-touch-startup-image" href="">
+        <link rel="apple-touch-icon" type="image/png" href="http://firesearch.co/api/imgs/FiRe.png">
+
+        <link rel="shortcut icon" type="image/ico" href="http://firesearch.co/api/imgs/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="http://firesearch.co/css/style.css">
+
+        <div id="custom_style" type="text/css">
+            <style>
+            </style>
+        </div>
+    </head>
+    <body>
+        <div class="header">
+            <header>
+                <center>
+                  <a href="http://firesearch.co/login" class="blue-background login button">Login</a>
+                  <a href="http://firesearch.co/signup" class="blue-background signup button">Signup</a>
+                </center>
+            </header>
+        </div>
+        <hr>
+        <div class="main_body">
+            <center>
+                <img class="wide-logo-main" src="http://localhost/api/imgs/logo-wide.png" width=384px; height=216px;></img>
+                <div class="search-bar">
+                  <!-- Note: add finish the search bar input to db (by Ariston Thomas Lorenzo) -->
+                  <input type="text" name="search-bar" value="Search the web..." size="60">
+                </div>
+            </center>
+        </div>
+        <hr>
+        <div class="footer">
+          <footer>
+            <center>
+              <p class="copyright_footer"> Copyright © 2019 FiRe Technologies LLC. All rights reserved. </p>
+            </center>
+          </footer>
+        </div>
+    </body>
+</html>
+</doctype>
---
+++ login/index.php
@@ -0,0 +1,79 @@
+<!--
+ ________ ___  ________  _______
+|\  _____\\  \|\   __  \|\  ___ \
+\ \  \__/\ \  \ \  \|\  \ \   __/|          index.php
+ \ \   __\\ \  \ \   _  _\ \  \_|/__
+  \ \  \_| \ \  \ \  \\  \\ \  \_|\ \       Updated by Ariston Lorenzo on 6/18/2019.
+   \ \__\   \ \__\ \__\\ _\\ \_______\
+    \|__|    \|__|\|__|\|__|\|_______|      Copyright © 2019 FiRe Tech (on behalf of 'myst development' and 'LScar Studios').
+-->
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="origin" name="referrer">
+        <meta name="keywords" content="FiRe, fire, firesearch, FiReSearch, FireSearch, FiReTech, FireTech, FiReTechnologies, FireTechnologies, FiRe Technologies, Fire Technologies">
+        <meta name="description" content="Search the web with a safe and modern engine!">
+        <meta name="subject" content="A search engine made to give you friendly an safe user experience.">
+        <meta name="copyright" content="<copyright>Copyright © 2019 FiRe Technologies (on behalf of 'myst development' and 'LScar Studios'). All rights reserved.</copyright>">
+        <meta name="language" content="english">
+        <meta name="author" content="Ariston Lorenzo, ariston.thomas@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Ariston Thomas Lorenzo, CEO">
+        <meta name="author" content="Levi Scott Hicks, tbnrscar@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Levi Scott Hicks, CTO">
+        <meta name="designer" content="Ariston Lorenzo, ariston.thomas@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Ariston Thomas Lorenzo, CEO">
+        <meta name="reply-to" content="mail@firesearch.co">
+        <meta name="owner" content="Ariston Lorenzo, ariston.thomas@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Ariston Thomas Lorenzo, CEO">
+        <meta name="owner" content="Levi Scott Hicks, tbnrscar@gmail.com or mail@firesearch.co; if you are using the FiRe email, please address to Levi Scott Hicks, CTO">
+        <meta name="url" content="http://firesearch.co/login">
+        <meta name="indentifier-url" content="http://firesearch.co">
+
+        <!-- Apple Metadata -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-touch-fullscreen" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="blue">
+        <meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=320, initial-scale=2.3, user-scalable=no">
+        <meta name="viewport" content="width = device-width">
+        <meta name="viewport" content="initial-scale = 1.0">
+        <meta name="viewport" content="initial-scale = 2.3, user-scalable=no">
+
+        <!-- Internet Explorer Metadata -->
+        <meta http-equiv="Page-Enter" content="RevealTrans(Duration=2.0,Transition=2)">
+        <meta http-equiv="Page-Exit" content="RevealTrans(Duration=3.0,Transition=12)">
+        <meta name="mssmarttagspreventparsing" content="true">
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
+        <meta name="msapplication-starturl" content="http://firesearch.co">
+        <meta name="msapplication-window" content="width=800;height=600">
+        <meta name="msapplication-navbutton-color" content="red">
+        <meta name="application-name" content="FiReSearch">
+        <meta name="msapplication-tooltip" content="Launch FiReSearch">
+
+        <title> FiRe - Log In </title>
+
+        <link rel="apple-touch-icon" href="http://localhost/api/imgs/FiRe.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="http://localhost/api/imgs/FiRe.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="http://localhost/api/imgs/FiRe.png">
+        <link rel="apple-touch-startup-image" href="">
+        <link rel="apple-touch-icon" type="image/png" href="http://localhost/api/imgs/FiRe.png">
+
+        <link rel="shortcut icon" type="image/ico" href="http://localhost/api/imgs/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="http://localhost/api/css/style.css">
+
+        <div id="custom_style" type="text/css">
+            <style>
+            </style>
+        </div>
+    </head>
+    <body>
+        <div class="header">
+            <header>
+                
+            </header>
+        </div>
+        <div class="main_body">
+            <center>
+            </center>
+        </div>
+    </body>
+</html>
+</doctype>
